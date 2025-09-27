c23772bcf731ce9e214fbd56c75aaef62dd7fec2
luancardoso94 <luancardoso100@hotmail.com>
2025-09-27 17:50:08 -0300

    novav2

--- prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-
-// prisma.config.ts (localizado na raiz do projeto)
-import { defineConfig } from '@prisma/cli';
-
-export default defineConfig({
-  seed: 'ts-node --compiler-options {"module":"CommonJS"} prisma/seed.ts',
-});
